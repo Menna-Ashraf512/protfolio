5031a7ad41715b33887461460c343ae5e7391a32
Menna-Ashraf512 <mennaalla@gmail.com>
2025-07-11 18:14:11 +0300

    update design

--- src/app/core/pages/projects/projects.component.ts
+++ src/app/core/pages/projects/projects.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-projects',
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      gsap.utils.toArray('.project-box').forEach((elem: any, index: number) => {
+        const fromDirection = index % 2 === 0 ? 100 : -100; // even → right, odd → left
+
+        gsap.from(elem, {
+          scrollTrigger: {
+            trigger: elem,
+            start: 'top 85%',
+            toggleActions: 'play reverse play reverse',
+          },
+          opacity: 0,
+          x: fromDirection,
+          duration: 1,
+          ease: 'power2.out',
+        });
+      });
+    }
+  }
 }
